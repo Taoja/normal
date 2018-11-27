87e4f817a48b22ba17960f8111bfce55e41cb0e4
huangwutao <346792184@qq.com>
2018-11-27 17:49:32 +0800

    框架变更

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const path = require('path')
 function resolve (e) {
@@ -42,6 +43,12 @@ const config = {
     },
     plugins: [
       new VueLoaderPlugin(), //vue加载器
+      new CopyWebpackPlugin([
+        { 
+          from:  resolve('static'), 
+          to: resolve(`dist/static`)
+        }
+      ])
     ],
     loader: [
       {
